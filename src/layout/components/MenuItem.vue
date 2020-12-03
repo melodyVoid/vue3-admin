8ab0c42511c37ba28e2559840138a72b518ba76e
melodyVoid <botaoxyz@gmail.com>
2020-12-04 00:48:09 +0800

    feat: 布局侧边栏

---
+++ src/layout/components/MenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <template v-if="menu.children && menu.children.length > 0">
+    <a-sub-menu
+      v-for="submenu of menu.children"
+      :key="submenu.name"
+      :title="menu.meta?.title"
+    >
+      <MenuItem :menu="submenu"></MenuItem>
+    </a-sub-menu>
+  </template>
+  <a-menu-item v-else :key="menu.name">
+    <span>{{ menu.meta?.title }}</span>
+  </a-menu-item>
+</template>
+<script lang="ts">
+export default {
+  name: 'MenuItem',
+}
+</script>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { PropType } from 'vue'
+import type { RouteRecordRaw } from 'vue-router'
+
+const props = defineProps({
+  menu: {
+    type: Object as PropType<RouteRecordRaw>,
+    default: () => ({
+      name: '',
+      meta: { title: '' },
+      children: [],
+    }),
+  },
+})
+</script>
+<style lang="scss" scoped></style>
--- src/layout/components/Sidebar.vue
+++ src/layout/components/Sidebar.vue
@@ -1,36 +1,23 @@
 <template>
   <div>
-    <a-menu mode="inline">
-      <a-sub-menu v-for="(menu, index) of routes" :key="index">
-        <template #title>
-          {{ menu?.meta?.title }}
-        </template>
-        <template v-if="menu.children && menu.children.length > 0">
-          <a-sub-menu
-            v-for="(submenu, subIndex) of menu.children"
-            :key="subIndex"
-          >
-            <a-menu-item key="2">{{ submenu.meta.title }}</a-menu-item>
-          </a-sub-menu>
-        </template>
-
-        <a-sub-menu key="sub3" title="Submenu" v-else>
-          <a-menu-item key="7">
-            Option 7
-          </a-menu-item>
-          <a-menu-item key="8">
-            Option 8
-          </a-menu-item>
-        </a-sub-menu>
-      </a-sub-menu>
+    <a-menu mode="inline" theme="dark">
+      <MenuItem v-for="menu of routes" :key="menu.name" :menu="menu"></MenuItem>
     </a-menu>
   </div>
 </template>
 <script setup>
+import MenuItem from './MenuItem.vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 console.log(router.options.routes)
-const routes = router.options.routes
+// 过滤掉登录页等其他 layout 之外的路由
+const routes = router.options.routes.filter(
+  item => item.children && item.children.length > 0
+)
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ant-menu-dark.ant-menu-root.ant-menu {
+  height: 100%;
+}
+</style>
