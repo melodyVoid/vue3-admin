9fff7b165802d121417f2936a11ea6c0e11335c5
薄涛 <botaoxyz@gmail.com>
2020-12-08 18:54:58 +0800

    feat: 删除标签页逻辑

--- src/layout/components/MenuTabs.vue
+++ src/layout/components/MenuTabs.vue
@@ -5,6 +5,7 @@
       type="editable-card"
       hide-add
       @change="handleChangeMenuTab"
+      @edit="handleCloseMenuTab"
     >
       <a-tab-pane key="Dashboard" tab="首页" :closable="false"></a-tab-pane>
       <a-tab-pane
@@ -37,12 +38,16 @@ const menuTab = (route: RouteLocationNormalized): MenuTabRaw => ({
  * 高亮的tab
  */
 const activeKey = ref<string | symbol>(route.name)
+// 缓存被删除的 tabKey，用于在下面 watchEffect 的时候不向 mentTabs 里 push 数据
+const removeKey = ref<string | symbol>('')
 
 const menuTabs = computed(() => store.state.app.menuTabs)
 
 watchEffect(() => {
   activeKey.value = route.name
-  if (route.name === 'Dashboard') {
+  if (route.name === 'Dashboard' || route.name === removeKey.value) {
+    // 这里要清空一下，不然下次点击 removeKey 对应的菜单时不会向 menuTabs 里添加数据
+    removeKey.value = ''
     return
   }
   if (menuTabs.value.find(item => item.name === route.name) === undefined) {
@@ -50,8 +55,35 @@ watchEffect(() => {
   }
 })
 
+/**
+ * 切换 tab
+ */
 const handleChangeMenuTab = (activeKey: string) => {
   router.push({ name: activeKey })
+}
+
+/**
+ * 关闭 tab
+ */
+const handleCloseMenuTab = (targetKey: string, action: 'remove' | 'add') => {
+  // 缓存 removeKey
+  removeKey.value = targetKey
+  if (action === 'remove') {
+    const targetIndex = menuTabs.value.findIndex(item => item.name === targetKey)
+    const leftTabs = menuTabs.value.slice(0, targetIndex)
+    const rightTabs = menuTabs.value.slice(targetIndex + 1)
+    const tabs = [...leftTabs, ...rightTabs]
+    // 如果关闭当前页面
+    if (targetKey === activeKey.value) {
+      if (tabs.length === 0) {
+        router.push({ name: 'Dashboard' })
+      } else {
+        router.push({ name: tabs[tabs.length - 1].name })
+      }
+    }
+    store.commit('app/SET_MENU_TABS', tabs)
+  }
+
 }
 </script>
 <style lang="scss" scoped>
