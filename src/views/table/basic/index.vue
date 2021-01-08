b30fd382694e2ee7c94efb4ccac7941b64f1513a
melodyVoid <botaoxyz@gmail.com>
2021-01-08 16:28:03 +0800

    feat: 删除

--- src/views/table/basic/index.vue
+++ src/views/table/basic/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a-table :columns="columns" :data-source="data">
+    <a-table :columns="columns" :data-source="data" bordered>
       <template #name="{ text }">
         <span>{{ text }}</span>
       </template>
@@ -27,7 +27,13 @@
         <span>
           <a> Invite - {{ record.name }}</a>
           <a-divider type="vertical"></a-divider>
-          <a>Delete</a>
+          <a-popconfirm
+            v-if="data.length"
+            title="确定要删除该条数据吗？"
+            @confirm="handleDelete(record)"
+          >
+            <a>Delete</a>
+          </a-popconfirm>
           <a-divider type="vertical"></a-divider>
           <a>More actions <down-outlined /></a>
         </span>
@@ -36,31 +42,40 @@
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue'
 const columns = [
   {
     dataIndex: 'name',
     key: 'name',
     slots: { title: 'customTitle', customRender: 'name' },
+    align: 'center',
   },
-  { title: 'Age', dataIndex: 'age', key: 'age' },
-  { title: 'Address', dataIndex: 'address', key: 'address' },
+  { title: 'Age', dataIndex: 'age', key: 'age', align: 'center' },
+  { title: 'Address', dataIndex: 'address', key: 'address', align: 'center' },
   {
     title: 'Tags',
     key: 'tags',
     dataIndex: 'tags',
     slots: { customRender: 'tags' },
+    align: 'center',
   },
-  { title: 'Action', key: 'action', slots: { customRender: 'action' } },
+  {
+    title: 'Action',
+    key: 'action',
+    slots: { customRender: 'action' },
+    align: 'center',
+  },
 ]
 
-const data = [
+const data = ref([
   {
     key: '1',
     name: 'John Brown',
     age: 32,
     address: 'New York No. 1 Lake Park',
     tags: ['nice', 'developer'],
+    title: '标题',
   },
   {
     key: '2',
@@ -76,6 +91,11 @@ const data = [
     address: 'Sidney No. 1 Lake Park',
     tags: ['cool', 'teacher'],
   },
-]
+])
+
+const handleDelete = (record: any) => {
+  console.log(record)
+  data.value = data.value.filter(item => item.key !== record.key)
+}
 </script>
 <style lang="scss" scoped></style>
